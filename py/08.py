0ee2b451ea06e0355a8d8b2b3990c46a405da045
andrew <Andrew.Kostousov@gmail.com>
2016-01-05 03:38:26 +0500

    day 8 - part 2

--- py/08.py
+++ py/08.py
@@ -1,4 +1,5 @@
 result = 0
 for line in open('08.in').read().splitlines():
-    result += len(line) - len(eval(line))
+    # result += len(line) - len(eval(line))
+    result += 2 + line.count('\\') + line.count('\"')
 print result
